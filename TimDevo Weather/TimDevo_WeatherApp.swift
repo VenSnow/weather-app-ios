005e17cfd517a36766cda4758903d030eace118e
VenSnow <timhale2104@icloud.com>
2023-05-16 17:25:51 +0500

    Initial Commit

---
+++ TimDevo Weather/TimDevo_WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimDevo_WeatherApp.swift
+//  TimDevo Weather
+//
+//  Created by Timur Gabdurashitov on 16/05/23.
+//
+
+import SwiftUI
+
+@main
+struct TimDevo_WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
